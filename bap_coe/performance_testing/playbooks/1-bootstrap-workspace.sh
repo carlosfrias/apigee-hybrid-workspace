9a1499682e622d48ef36b512ec60bb8c47dce15a
Carlos Frias <friasc@google.com>
2021-03-10 22:16:30 +0000

    updated config

--- bap_coe/performance_testing/playbooks/1-bootstrap-workspace.sh
+++ bap_coe/performance_testing/playbooks/1-bootstrap-workspace.sh
@@ -2,7 +2,11 @@
 sudo apt update -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
+software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install python3.8
 curl https://pyenv.run | bash
 export PATH=~/.pyenv/bin:$PATH
 pyenv install 3.9.1
