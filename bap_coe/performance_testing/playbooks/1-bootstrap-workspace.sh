215660dfd76b72b3814b4bcde010f0b20b40dd83
Carlos Frias <friasc@google.com>
2021-03-16 15:16:59 +0000

    updated config

--- bap_coe/performance_testing/playbooks/1-bootstrap-workspace.sh
+++ bap_coe/performance_testing/playbooks/1-bootstrap-workspace.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends tzdata
-sudo apt update -y
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+export DEBIAN_FRONTEND="noninteractive apt install -y --no-install-recommends tzdata"
+apt update -y
+apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
 software-properties-common
-sudo add-apt-repositor -y ppa:deadsnakes/ppa
-sudo apt update -y
-sudo apt install python3
+add-apt-repositor -y ppa:deadsnakes/ppa
+apt update -y
+apt install python3
 curl https://pyenv.run | bash
 export PATH=~/.pyenv/bin:$PATH
 echo "PATH=~/.pyenv/bin:$PATH" >> ~/.bashrc
