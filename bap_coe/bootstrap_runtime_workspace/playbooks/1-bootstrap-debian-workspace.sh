1c500029e7cb9f674b26d3a1d4b46ff0da02f95c
Carlos Frias <friasc@google.com>
2021-03-17 15:23:01 +0000

    updated bootstrap config"

--- bap_coe/bootstrap_runtime_workspace/playbooks/1-bootstrap-debian-workspace.sh
+++ bap_coe/bootstrap_runtime_workspace/playbooks/1-bootstrap-debian-workspace.sh
@@ -4,5 +4,5 @@ apt update -y
 apt-get install -y sudo make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
-software-properties-common git
+software-properties-common git python3-apt
 apt update -y
