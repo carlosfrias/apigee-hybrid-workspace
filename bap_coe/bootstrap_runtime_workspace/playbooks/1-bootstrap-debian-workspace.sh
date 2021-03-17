dbc6b24eea0f0f35e20dc333599c7639e3f463e6
Carlos Frias <friasc@google.com>
2021-03-17 20:20:06 +0000

    update config

--- bap_coe/bootstrap_runtime_workspace/playbooks/1-bootstrap-debian-workspace.sh
+++ bap_coe/bootstrap_runtime_workspace/playbooks/1-bootstrap-debian-workspace.sh
@@ -4,5 +4,6 @@ apt update -y
 apt-get install -y sudo make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
-software-properties-common git python3-apt
+software-properties-common git python3-apt apt-transport-https \
+ca-certificates gnupg
 apt update -y
