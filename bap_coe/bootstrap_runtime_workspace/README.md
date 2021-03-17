e1fed9e42811272ea42c78d786e3ce5520abed06
Carlos Frias <friasc@google.com>
2021-03-17 13:46:01 +0000

    updated config for bootstraping environments

---
+++ bap_coe/bootstrap_runtime_workspace/README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - bap_coe.bootstrap_workspace
+
+Documentation for the collection.
--- bap_coe/bootstrap_runtime_workspace/playbooks/1-bootstrap-debian-workspace.sh
+++ bap_coe/bootstrap_runtime_workspace/playbooks/1-bootstrap-debian-workspace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DEBIAN_FRONTEND="noninteractive apt-get install -y --no-install-recommends tzdata"
 apt update -y
-apt-get install -y make build-essential libssl-dev zlib1g-dev \
+apt-get install -y sudo make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
 software-properties-common git
