5dc23b52adc4616a6ad5a5fd20acd6a7666c1408
Carlos Frias <friasc@google.com>
2021-03-18 02:44:45 +0000

    updated config

--- bap_coe/bootstrap_runtime_workspace/playbooks/3-bootstrap-pyenv-virtualenv.sh
+++ bap_coe/bootstrap_runtime_workspace/playbooks/3-bootstrap-pyenv-virtualenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pyenv virtualenv 3.9.1 ups_perf_test
-pyenv activate ups_perf_test
+pyenv virtualenv 3.9.1 bap_coe
+pyenv activate bap_coe
 pip3 install -r requirements.txt
 ansible-galaxy install -r collections.yml --force
