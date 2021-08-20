5439abacbd1160a12acf9e9075d4710c2fa940c5
Carlos Frias <friasc@google.com>
2021-08-20 15:37:23 +0000

    updated bootstrap runtime

--- bap_coe/bootstrap_runtime/playbooks/setup.sh
+++ bap_coe/bootstrap_runtime/playbooks/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+sudo apt update -y
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 curl https://pyenv.run | bash
