c7bd86a77d4fef3aa6b0099a88872eca36ce674c
Carlos Frias <friasc@google.com>
2021-08-20 15:35:28 +0000

    updated bootstrap runtime

--- bap_coe/bootstrap_runtime/playbooks/setup.sh
+++ bap_coe/bootstrap_runtime/playbooks/setup.sh
@@ -1,4 +1,6 @@
 #! bash
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 curl https://pyenv.run | bash
 pyenv install 3.9.1
-sudo apt install -y python3-apt
