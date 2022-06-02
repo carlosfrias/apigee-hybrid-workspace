f750567a7283b59adca3732835ce0c77cc7d83de
Carlos Frias <friasc@google.com>
2022-06-02 15:38:43 +0000

    setup workspace

--- setup-apigee-workspace.sh
+++ setup-apigee-workspace.sh
@@ -4,9 +4,9 @@ echo "Setup apigee-workspace"
 
 curl https://pyenv.run | bash
 export PATH=~/.pyenv/bin:$PATH
-echo "export PATH=\"~/.pyenv/bin:$PATH\"" >> ~/.bashrc
-echo "eval \"$(pyenv init -)\"" >> ~/.bashrc
-echo "eval \"$(pyenv virtualenv-init -)\"" >> ~/.bashrc
+echo 'export PATH=\"~/.pyenv/bin:$PATH\"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
 pyenv install 3.9.1
 
