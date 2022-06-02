d74e24baaed9efc35977596ec3baf29aa58348c0
Carlos Frias <friasc@google.com>
2022-06-02 15:13:58 +0000

    setup workspace

--- bap_coe/bootstrap_runtime/playbooks/setup.sh
+++ bap_coe/bootstrap_runtime/playbooks/setup.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-echo "Please run the bootstrap scripts as needed: 1-bootstrap-debian-workspace.sh, 2-bootstrap-pyenv.sh or 3-bootstrap-pyenv-virtualenv.sh"
+echo "Please run the bootstrap scripts as needed: "
+echo "    1-bootstrap-debian-workspace.sh"
+echo "    2-bootstrap-pyenv.sh"
+echo "    3-bootstrap-pyenv-virtualenv.sh"
