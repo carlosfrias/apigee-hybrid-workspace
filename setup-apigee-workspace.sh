0fef88eba57f554850239d1412d41b6c31dcfd27
Carlos Frias <friasc@google.com>
2022-06-02 15:56:38 +0000

    setup workspace

--- setup-apigee-workspace.sh
+++ setup-apigee-workspace.sh
@@ -9,4 +9,6 @@ echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
 pyenv install 3.9.1
-
+pyenv virtualenv 3.9.1 bap_coe
+pyenv activate bap_coe
+pip3 install ansible 
