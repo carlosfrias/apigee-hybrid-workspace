cbd1945d7185a8ea6c7e3f081f4c243ce5b19f21
Carlos Frias <friasc@google.com>
2022-08-30 18:19:40 +0000

    updated setup

--- bap_coe/apigee_x_terraform_modules/playbooks/configure-workspace/prepare-system.sh
+++ bap_coe/apigee_x_terraform_modules/playbooks/configure-workspace/prepare-system.sh
@@ -24,7 +24,7 @@ install_pip() {
 prepare_dev() {
     $SUDO apt update -y
     $SUDO apt dist-upgrade -y
-#    $SUDO apt install software-properties-common curl git mc vim facter aptitude -y
+    $SUDO apt install software-properties-common curl git mc vim facter aptitude -y
 #    [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc libffi-dev libssl-dev make -y
 
     PYTHON_BIN=/usr/bin/python
@@ -105,7 +105,7 @@ prepare_amzn() {
 usage() {
   echo
   echo "Linux distribution not detected."
-  echo "Use: ID=[ubuntu|debian|centos|raspbian|amzn|fedora] prepare_system.sh"
+  echo "Use: ID=[ubuntu|debian|centos|raspbian|amzn|fedora|dev] prepare_system.sh"
   echo "Other distributions not yet supported."
   echo
 }
