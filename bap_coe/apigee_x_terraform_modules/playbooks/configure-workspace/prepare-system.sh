eb654757d5abdd199bd7f14ee7365f9e35251145
Carlos Frias <friasc@google.com>
2022-08-30 17:33:15 +0000

    updated setup

--- bap_coe/apigee_x_terraform_modules/playbooks/configure-workspace/prepare-system.sh
+++ bap_coe/apigee_x_terraform_modules/playbooks/configure-workspace/prepare-system.sh
@@ -18,8 +18,8 @@ install_pip() {
 prepare_dev() {
     $SUDO apt update -y
     $SUDO apt dist-upgrade -y
-    $SUDO apt install software-properties-common curl git mc vim facter python3-apt aptitude -y
-    [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
+    $SUDO apt install software-properties-common curl git mc vim facter aptitude -y
+    [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc libffi-dev libssl-dev make -y
 
     PYTHON_BIN=/usr/bin/python
     install_pyenv
